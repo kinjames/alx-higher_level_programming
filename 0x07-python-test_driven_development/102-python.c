36f59efdee33267b7d0c67f3f7e45e37eae548ab
James Kofi Myers <myers_james59@yahoo.com>
2023-01-27 12:48:24 -0800

    Task 11

---
+++ 0x07-python-test_driven_development/102-python.c
@@ -0,0 +1,22 @@
+#include "Python.h"
+
+/**
+ * print_python_string - Prints information about Python strings.
+ * @p: A PyObject string object.
+ */
+void print_python_string(PyObject *p)
+{
+	long int length;
+
+	fflush(stdout);
+
+	printf("[.] string object info\n");
+	if (strcmp(p->ob_type->tp_name, "str") != 0)
+	{
+		printf("  [ERROR] Invalid String Object\n");
+		return;
+	}
+
+	length = ((PyASCIIObject *)(p))->length;
+
+	if (PyUnicode_IS_COMPACT_ASCII(p))
