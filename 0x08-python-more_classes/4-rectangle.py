feffda0dba2cf56f46180dc3e329eb0bc6755460
James Kofi Myers <myers_james59@yahoo.com>
2023-01-30 08:44:18 -0800

    Task 5

---
+++ 0x08-python-more_classes/4-rectangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Defines a class Rectangle
+"""
+
+
+class Rectangle:
+    """Representation of a rectangle"""
+    def __init__(self, width=0, height=0):
+        """Initializes the rectangle"""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """getter for the private instance attribute width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
