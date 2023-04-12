5ee07ddec876e2512d2eff116e364ee310906d63
James Kofi Myers <95756936+kinjames@users.noreply.github.com>
2023-04-12 20:16:47 +0000

    Update 10-factorial.js

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,7 +1,6 @@
-
 #!/usr/bin/node
-function factorial (a) {
-  return a === 0 || isNaN(a) ? 1 : a * factorial(a - 1);
+function factorial (n) {
+  return n === 0 || isNaN(n) ? 1 : n * factorial(n - 1);
 }
 
 console.log(factorial(Number(process.argv[2])));
