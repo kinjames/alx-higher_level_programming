a6bb280f78318861493746ec51dea0cd546bc78a
James Kofi Myers <95756936+kinjames@users.noreply.github.com>
2023-04-12 20:07:20 +0000

    Update 6-multi_languages_loop.js

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,19 +1,4 @@
 #!/usr/bin/node
-const languages = ["C", "Python", "JavaScript"];
-
-for (let i = 0; i < languages.length; i++) {
-  console.log(`${languages[i]} is ${getLanguageAdjective(languages[i])}`);
-}
-
-function getLanguageAdjective(language) {
-  switch (language) {
-    case "C":
-      return "fun";
-    case "Python":
-      return "cool";
-    case "JavaScript":
-      return "amazing";
-    default:
-      return "";
-  }
+for (const language of ['C is fun', 'Python is cool', 'JavaScript is amazing']) {
+  console.log(language);
 }
