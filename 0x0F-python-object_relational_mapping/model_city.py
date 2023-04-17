8b3e03c1053e292c6bf0fab13e5397280d910279
James Kofi Myers <myers_james59@yahoo.com>
2023-04-17 04:40:39 -0700

    update

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """
+This script defines a City class
+to work with MySQLAlchemy ORM.
 """
 
-from relationship_state import Base, State
+from model_state import Base, State
 from sqlalchemy import Column, Integer, String, ForeignKey
 
 
